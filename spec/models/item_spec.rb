c524d99a8042e4df640cb2faba3a5d5789ac7657
Jack Yeh <jack.yeh@gmail.com>
2015-07-28 10:02:21 -0600

    pass/create test: Item has many relationships: - has_many invoice_items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:merch) do
+    Merchant.create!(name: "merch1")
+  end
+
+  let(:customer) do
+    Customer.create!(first_name: "jack", last_name: "yeh")
+  end
+
+  let(:invoice) do
+    Invoice.create!(customer_id: customer.id, merchant_id: merch.id, status: "does it really matter")
+  end
+
+  describe 'has many relationships' do
+    it 'has many invoice_items' do
+      item = Item.create!(name: "item1", description: "desc1", unit_price: 19, merchant_id: merch.id)
+
+      expect(item.invoice_items.count).to eq(0)
+
+      invoice_item1 = InvoiceItem.create!(invoice_id: invoice.id, item_id: item.id, quantity: 1)
+      invoice_item2 = InvoiceItem.create!(invoice_id: invoice.id, item_id: item.id, quantity: 2)
+
+      expect(item.invoice_items.count).to eq(2)
+    end
 end
