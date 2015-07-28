f750e60f316ddd1ca97d8eea0537bbb613429442
Jack Yeh <jack.yeh@gmail.com>
2015-07-27 19:07:47 -0600

    create/pass tests: Invoice has_many relationships: - has_many transactions - has_many invoice_items - has_many items

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:merch) do
+    Merchant.create!(name: "merch1")
+  end
+
+  let(:customer) do
+    Customer.create!(first_name: "jack", last_name: "yeh")
+  end
+
+  let(:invoice) do
+    Invoice.create!(customer_id: customer.id, merchant_id: merch.id, status: "does it really matter")
+  end
+
+  describe 'has many relationships' do
+    it 'has many transactions' do
+      expect(invoice.transactions.count).to eq(0)
+      trans1 = Transaction.create!(invoice_id: invoice.id)
+      trans2 = Transaction.create!(invoice_id: invoice.id)
+      expect(invoice.transactions.count).to eq(2)
+    end
+
+    it 'has many invoice_items' do
+      item1 = Item.create!(name: "item1", description: "desc1", unit_price: 19, merchant_id: merch.id)
+      item2 = Item.create!(name: "item2", description: "desc2", unit_price: 29, merchant_id: merch.id)
+
+      expect(invoice.invoice_items.count).to eq(0)
+
+      invoice_item1 = InvoiceItem.create!(invoice_id: invoice.id, item_id: item1.id, quantity: 1)
+      invoice_item2 = InvoiceItem.create!(invoice_id: invoice.id, item_id: item2.id, quantity: 2)
+
+      expect(invoice.invoice_items.count).to eq(2)
+    end
+
+    it 'has many items through invoice_items' do
+      item1 = Item.create!(name: "item1", description: "desc1", unit_price: 19, merchant_id: merch.id)
+      item2 = Item.create!(name: "item2", description: "desc2", unit_price: 29, merchant_id: merch.id)
+
+      expect(invoice.items.count).to eq(0)
+
+      invoice_item1 = InvoiceItem.create!(invoice_id: invoice.id, item_id: item1.id, quantity: 1)
+      invoice_item2 = InvoiceItem.create!(invoice_id: invoice.id, item_id: item2.id, quantity: 2)
+
+      expect(invoice.items.count).to eq(2)
+    end
+  end
 end
