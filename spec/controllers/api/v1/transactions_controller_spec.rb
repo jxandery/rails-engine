323439428cf2cbc2a9286fd912a5a3129d43d817
Jack Yeh <jack.yeh@gmail.com>
2015-07-29 20:37:35 -0600

    spec/controllers/api/v1/transactions_controller_spec.rb
    fixed transactions#random

--- spec/controllers/api/v1/transactions_controller_spec.rb
+++ spec/controllers/api/v1/transactions_controller_spec.rb
@@ -32,4 +32,38 @@ RSpec.describe Api::V1::TransactionsController, type: :controller do
       expect(transaction['result']).to eq('success')
     end
   end
+
+  context '#random' do
+    it 'returns random transaction' do
+      Transaction.create!(invoice_id: 1, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      Transaction.create!(invoice_id: 2, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      Transaction.create!(invoice_id: 3, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      Transaction.create!(invoice_id: 4, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      Transaction.create!(invoice_id: 5, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      Transaction.create!(invoice_id: 6, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      Transaction.create!(invoice_id: 7, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      Transaction.create!(invoice_id: 8, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      Transaction.create!(invoice_id: 9, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      Transaction.create!(invoice_id: 10, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      get :random, format: :json
+
+      expect(response).to have_http_status(:ok)
+      transactions = JSON.parse(response.body)
+      expect(transactions.count).to eq(1)
+    end
+  end
+
+  context '#find' do
+    it 'returns specific transaction' do
+      transaction = Transaction.create!(invoice_id: 4, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      get :show, id: transaction.id, format: :json
+
+      expect(response).to have_http_status(:ok)
+      transaction_response = JSON.parse(response.body)
+      expect(transaction['invoice_id']).to eq(4)
+      expect(transaction['credit_card_number']).to eq('78')
+      expect(transaction['credit_card_expiration_date']).to eq('01011978')
+      expect(transaction['result']).to eq('success')
+    end
+  end
 end
