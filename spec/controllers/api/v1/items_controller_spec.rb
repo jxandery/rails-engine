049124a6e7d7d4ff740db3443f9538996a3d626a
Jack Yeh <jack.yeh@gmail.com>
2015-07-29 20:01:45 -0600

    create/pass test: items#index & items#show

--- spec/controllers/api/v1/items_controller_spec.rb
+++ spec/controllers/api/v1/items_controller_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ItemsController, type: :controller do
+  context '#index' do
+    it 'returns all the items' do
+      Item.create!(name: 'Austen', description: 'hunger academy alum', unit_price: 99, merchant_id: 88)
+      get :index, format: :json
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
+      items = JSON.parse(response.body)
+      expect(items.count).to eq(1)
+
+      item = items.first
+      expect(item['name']).to eq('Austen')
+      expect(item['description']).to eq('hunger academy alum')
+      expect(item['unit_price']).to eq(99)
+      expect(item['merchant_id']).to eq(88)
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  context '#show' do
+    it 'returns individual item' do
+      item = Item.create!(name: 'Austen', description: 'hunger academy alum', unit_price: 99, merchant_id: 88)
+      get :show, id: item.id, format: :json
+
+      expect(response).to have_http_status(:ok)
+      item_response = JSON.parse(response.body)
+      expect(item['name']).to eq('Austen')
+      expect(item['description']).to eq('hunger academy alum')
+      expect(item['unit_price']).to eq(99)
+      expect(item['merchant_id']).to eq(88)
     end
   end
-
 end
