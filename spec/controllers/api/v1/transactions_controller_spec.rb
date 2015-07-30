e312c69061e627d29a82a4d302508f32e23e22d3
Jack Yeh <jack.yeh@gmail.com>
2015-07-29 20:48:13 -0600

    fixed transactions#random & pass/create test: transactions#find

--- spec/controllers/api/v1/transactions_controller_spec.rb
+++ spec/controllers/api/v1/transactions_controller_spec.rb
@@ -49,21 +49,22 @@ RSpec.describe Api::V1::TransactionsController, type: :controller do
 
       expect(response).to have_http_status(:ok)
       transactions = JSON.parse(response.body)
-      expect(transactions.count).to eq(1)
+      expect(transactions).not_to eq(Transaction.all.sample)
     end
   end
 
   context '#find' do
     it 'returns specific transaction' do
-      transaction = Transaction.create!(invoice_id: 4, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
-      get :show, id: transaction.id, format: :json
+      transaction1 = Transaction.create!(invoice_id: 4, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      transaction2 = Transaction.create!(invoice_id: 2, credit_card_number: '9807', credit_card_expiration_date: '04041978', result: 'pending')
+      get :find, invoice_id: transaction1.invoice_id, format: :json
 
       expect(response).to have_http_status(:ok)
       transaction_response = JSON.parse(response.body)
-      expect(transaction['invoice_id']).to eq(4)
-      expect(transaction['credit_card_number']).to eq('78')
-      expect(transaction['credit_card_expiration_date']).to eq('01011978')
-      expect(transaction['result']).to eq('success')
+      expect(transaction1['invoice_id']).to eq(4)
+      expect(transaction1['credit_card_number']).to eq('78')
+      expect(transaction1['credit_card_expiration_date']).to eq('01011978')
+      expect(transaction1['result']).to eq('success')
     end
   end
 end
