15380e39955e42d848d93744a79f7bb93a0c8e35
Jack Yeh <jack.yeh@gmail.com>
2015-07-29 20:08:51 -0600

    create/pass test: invoices#index & invoices#show

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -1,19 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::InvoicesController, type: :controller do
+  context '#index' do
+    it 'returns all the invoices' do
+      Invoice.create!(customer_id: 99, merchant_id: 88, status: "fair")
+      get :index, format: :json
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
+      invoices = JSON.parse(response.body)
+      expect(invoices.count).to eq(1)
+
+      invoice = invoices.first
+      expect(invoice['customer_id']).to eq(99)
+      expect(invoice['merchant_id']).to eq(88)
+      expect(invoice['status']).to eq('fair')
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  context '#show' do
+    it 'returns individual invoice' do
+      invoice = Invoice.create!(customer_id: 99, merchant_id: 88, status: "fair")
+      get :show, id: invoice.id, format: :json
+
+      expect(response).to have_http_status(:ok)
+      invoice_response = JSON.parse(response.body)
+      expect(invoice['customer_id']).to eq(99)
+      expect(invoice['merchant_id']).to eq(88)
+      expect(invoice['status']).to eq('fair')
     end
   end
-
 end
