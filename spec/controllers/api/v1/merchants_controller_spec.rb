c3d7412970f9eb3c143e21e35980b86cb12631d3
Jack Yeh <jack.yeh@gmail.com>
2015-07-29 19:54:52 -0600

    create/pass test: merchants#index & merchants#show

--- spec/controllers/api/v1/merchants_controller_spec.rb
+++ spec/controllers/api/v1/merchants_controller_spec.rb
@@ -1,19 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::MerchantsController, type: :controller do
+  context '#index' do
+    it 'returns all the merchants' do
+      Merchant.create!(name: 'Austen')
+      get :index, format: :json
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
+      merchants = JSON.parse(response.body)
+      expect(merchants.count).to eq(1)
+
+      merchant = merchants.first
+      expect(merchant['name']).to eq('Austen')
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  context '#show' do
+    it 'returns individual merchant' do
+      merchant = Merchant.create!(name: 'Austen')
+      get :show, id: merchant.id, format: :json
+
+      expect(response).to have_http_status(:ok)
+      merchant_response = JSON.parse(response.body)
+      expect(merchant['name']).to eq('Austen')
     end
   end
-
 end
