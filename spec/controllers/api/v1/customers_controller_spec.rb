d858b72649a7e447bd55ece9da8f2a8fc53f32d0
Jack Yeh <jack.yeh@gmail.com>
2015-07-29 19:34:09 -0600

    create/pass test: customers#index

--- spec/controllers/api/v1/customers_controller_spec.rb
+++ spec/controllers/api/v1/customers_controller_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::CustomersController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  context '#index' do
+    it 'returns all the customers' do
+      Customer.create!(first_name: 'strawberry', last_name: 'red')
+      get :index, format: :json
+
+      expect(response).to have_http_status(:ok)
+      customers = JSON.parse(response.body)
+      expect(customers.count).to eq(1)
+
+      customer = customers.first
+      expect(customer['first_name']).to eq('strawberry')
+      expect(customer['last_name']).to eq('red')
     end
   end
 
