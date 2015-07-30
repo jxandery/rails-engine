c96aabbd53cbb62cb42b2752bc1a7f4a338b0680
Jack Yeh <jack.yeh@gmail.com>
2015-07-29 20:25:09 -0600

    create/pass test: invoice_items#index & invoice_items#show

--- spec/controllers/api/v1/invoice_items_controller_spec.rb
+++ spec/controllers/api/v1/invoice_items_controller_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::InvoiceItemsController, type: :controller do
+  context '#index' do
+    it 'returns all the invoice_items' do
+      InvoiceItem.create!(invoice_id: 99, item_id: 88, quantity: 100, unit_price: 77)
+      get :index, format: :json
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
+      invoice_items = JSON.parse(response.body)
+      expect(invoice_items.count).to eq(1)
+
+      invoice_item = invoice_items.first
+      expect(invoice_item['invoice_id']).to eq(99)
+      expect(invoice_item['item_id']).to eq(88)
+      expect(invoice_item['quantity']).to eq(100)
+      expect(invoice_item['unit_price']).to eq(77)
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  context '#show' do
+    it 'returns individual invoice_item' do
+      invoice_item = InvoiceItem.create!(invoice_id: 99, item_id: 88, quantity: 100, unit_price: 77)
+      get :show, id: invoice_item.id, format: :json
+
+      expect(response).to have_http_status(:ok)
+      invoice_item_response = JSON.parse(response.body)
+      expect(invoice_item['invoice_id']).to eq(99)
+      expect(invoice_item['item_id']).to eq(88)
+      expect(invoice_item['quantity']).to eq(100)
+      expect(invoice_item['unit_price']).to eq(77)
     end
   end
-
 end
