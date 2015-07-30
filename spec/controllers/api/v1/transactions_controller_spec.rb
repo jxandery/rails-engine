2f8096db8d809861d5eebe85a8455f161106afed
Jack Yeh <jack.yeh@gmail.com>
2015-07-29 19:49:07 -0600

    create/pass test: transactions#index & transactions#show

--- spec/controllers/api/v1/transactions_controller_spec.rb
+++ spec/controllers/api/v1/transactions_controller_spec.rb
@@ -2,18 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::TransactionsController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  context '#index' do
+    it 'returns all the transactions' do
+      Transaction.create!(invoice_id: 4, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      get :index, format: :json
+
+      expect(response).to have_http_status(:ok)
+      transactions = JSON.parse(response.body)
+      expect(transactions.count).to eq(1)
+
+      transaction = transactions.first
+      expect(transaction['invoice_id']).to eq(4)
+      expect(transaction['credit_card_number']).to eq('78')
+      expect(transaction['credit_card_expiration_date']).to eq('01011978')
+      expect(transaction['result']).to eq('success')
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  context '#show' do
+    it 'returns individual transaction' do
+      transaction = Transaction.create!(invoice_id: 4, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      get :show, id: transaction.id, format: :json
+
+      expect(response).to have_http_status(:ok)
+      transaction_response = JSON.parse(response.body)
+      expect(transaction['invoice_id']).to eq(4)
+      expect(transaction['credit_card_number']).to eq('78')
+      expect(transaction['credit_card_expiration_date']).to eq('01011978')
+      expect(transaction['result']).to eq('success')
     end
   end
-
 end
