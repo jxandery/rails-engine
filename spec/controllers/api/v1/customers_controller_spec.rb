ad063ceb422628dca78e82685b87a88a847e8179
Jack Yeh <jack.yeh@gmail.com>
2015-07-30 23:12:02 -0600

    create/pass test: Customers#favorite_merchant

--- spec/controllers/api/v1/customers_controller_spec.rb
+++ spec/controllers/api/v1/customers_controller_spec.rb
@@ -107,4 +107,24 @@ RSpec.describe Api::V1::CustomersController, type: :controller do
     end
   end
 
+  context '#favorite_merchant' do
+    it 'returns favorite_merchant' do
+      customer = Customer.create!(first_name: 'strawberry', last_name: 'red')
+      merchant1 = Merchant.create!(name: 'Jorge')
+      merchant2 = Merchant.create!(name: 'Austen')
+      invoice1 = Invoice.create!(customer_id: customer.id, merchant_id: merchant1.id, status: "success")
+      invoice2 = Invoice.create!(customer_id: customer.id, merchant_id: merchant1.id, status: "success")
+      invoice3 = Invoice.create!(customer_id: customer.id, merchant_id: merchant2.id, status: "success")
+      transaction1 = Transaction.create!(invoice_id: invoice1.id, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      transaction2 = Transaction.create!(invoice_id: invoice1.id, credit_card_number: '88', credit_card_expiration_date: '08081978', result: 'success')
+      transaction3 = Transaction.create!(invoice_id: invoice2.id, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      get :favorite_merchant, customer_id: customer.id
+
+      expect(response).to have_http_status(:ok)
+      favorite_merchant_response = JSON.parse(response.body)
+      expect(favorite_merchant_response.class).to eq(Hash)
+      expect(favorite_merchant_response['name']).to eq('Jorge')
+    end
+  end
+
 end
