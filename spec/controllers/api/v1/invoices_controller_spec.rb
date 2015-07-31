11d71c3138ec15a673edee45e60389a6d1798a4b
Jack Yeh <jack.yeh@gmail.com>
2015-07-30 21:51:47 -0600

    create/pass test: Invoice#customer

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -75,4 +75,17 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
     end
   end
 
+  context '#customer' do
+    it 'returns customer' do
+      customer = Customer.create!(first_name: 'strawberry', last_name: 'red')
+      invoice = Invoice.create!(customer_id: customer.id, merchant_id: 88, status: "fair")
+      get :customer, invoice_id: invoice.id
+
+      expect(response).to have_http_status(:ok)
+      customer_response = JSON.parse(response.body)
+      expect(customer_response.class).to eq(Hash)
+      expect(customer_response['first_name']).to eq('strawberry')
+      expect(customer_response['last_name']).to eq('red')
+    end
+  end
 end
