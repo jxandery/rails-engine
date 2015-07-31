bcd491365dff1720b36054cca926964585837589
Jack Yeh <jack.yeh@gmail.com>
2015-07-30 22:04:34 -0600

    create/pass test: Invoices#transactions & Invoices#invoice_items

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -101,4 +101,47 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
       expect(merchant_response['name']).to eq('Austen')
     end
   end
+
+  context '#transactions' do
+    it 'returns transactions' do
+      merchant = Merchant.create!(name: 'Austen')
+      customer = Customer.create!(first_name: 'strawberry', last_name: 'red')
+      invoice = Invoice.create!(customer_id: customer.id, merchant_id: merchant.id, status: "success")
+      transaction1 = Transaction.create!(invoice_id: invoice.id, credit_card_number: '78', credit_card_expiration_date: '01011978', result: 'success')
+      transaction2 = Transaction.create!(invoice_id: invoice.id, credit_card_number: '88', credit_card_expiration_date: '08081978', result: 'success')
+      get :transactions, invoice_id: invoice.id
+
+      expect(response).to have_http_status(:ok)
+      transactions_response = JSON.parse(response.body)
+      expect(transactions_response.count).to eq(2)
+      expect(transactions_response.first['invoice_id']).to eq(invoice.id)
+      expect(transactions_response.first['credit_card_number']).to eq('78')
+      expect(transactions_response.second['invoice_id']).to eq(invoice.id)
+      expect(transactions_response.second['credit_card_number']).to eq('88')
+    end
+  end
+
+  context '#invoice_items' do
+    it 'returns invoice_items' do
+      merchant = Merchant.create!(name: 'Austen')
+      item = Item.create!(name: 'item9', description: 'description9', unit_price: 99, merchant_id: merchant.id)
+      customer = Customer.create!(first_name: 'strawberry', last_name: 'red')
+      invoice1 = Invoice.create!(customer_id: customer.id, merchant_id: merchant.id, status: "success")
+      invoice_items1 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item.id, quantity: 100, unit_price: 77)
+      invoice_items2 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item.id, quantity: 111, unit_price: 11)
+      get :invoice_items, invoice_id: invoice1.id
+
+      expect(response).to have_http_status(:ok)
+      invoice_items_response = JSON.parse(response.body)
+      expect(invoice_items_response.count).to eq(2)
+      expect(invoice_items_response.first['invoice_id']).to eq(invoice1.id)
+      expect(invoice_items_response.first['item_id']).to eq(item.id)
+      expect(invoice_items_response.first['quantity']).to eq(100)
+      expect(invoice_items_response.first['unit_price']).to eq(77)
+      expect(invoice_items_response.second['invoice_id']).to eq(invoice1.id)
+      expect(invoice_items_response.second['item_id']).to eq(item.id)
+      expect(invoice_items_response.second['quantity']).to eq(111)
+      expect(invoice_items_response.second['unit_price']).to eq(11)
+    end
+  end
 end
