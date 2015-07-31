f7592f575c4af9fb5d48fd63b32e2c29c69d53ee
Jack Yeh <jack.yeh@gmail.com>
2015-07-31 08:04:40 -0600

    create/pass test: Merchants#random

--- spec/controllers/api/v1/merchants_controller_spec.rb
+++ spec/controllers/api/v1/merchants_controller_spec.rb
@@ -25,4 +25,24 @@ RSpec.describe Api::V1::MerchantsController, type: :controller do
       expect(merchant['name']).to eq('Austen')
     end
   end
+
+  context '#random' do
+    it 'returns random customer' do
+      Merchant.create!(name: 'Auste1')
+      Merchant.create!(name: 'Auste2')
+      Merchant.create!(name: 'Auste3')
+      Merchant.create!(name: 'Auste4')
+      Merchant.create!(name: 'Auste5')
+      Merchant.create!(name: 'Auste6')
+      Merchant.create!(name: 'Auste7')
+      Merchant.create!(name: 'Auste8')
+      Merchant.create!(name: 'Auste9')
+      get :random, format: :json
+
+      expect(response).to have_http_status(:ok)
+      merchant_response = JSON.parse(response.body)
+      expect(merchant_response).not_to eq(Merchant.all.sample)
+    end
+  end
+
 end
