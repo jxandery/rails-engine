10326a98c36381fc1d788be162b8e26fbd02e5a3
Jack Yeh <jack.yeh@gmail.com>
2015-07-31 08:25:44 -0600

    create/pass test: Merchants#invoices

--- spec/controllers/api/v1/merchants_controller_spec.rb
+++ spec/controllers/api/v1/merchants_controller_spec.rb
@@ -69,4 +69,21 @@ RSpec.describe Api::V1::MerchantsController, type: :controller do
       expect(merchant_response.count).to eq(2)
     end
   end
+
+  context '#invoices' do
+    it 'returns invoices' do
+      merchant = Merchant.create!(name: 'Auste8')
+      customer = Customer.create!(first_name: 'strawberry', last_name: 'red')
+      invoice1 = Invoice.create!(customer_id: customer.id, merchant_id: merchant.id, status: "success")
+      invoice2 = Invoice.create!(customer_id: customer.id, merchant_id: merchant.id, status: "success")
+      get :invoices, merchant_id: merchant.id
+
+      expect(response).to have_http_status(:ok)
+      invoice_response = JSON.parse(response.body)
+      expect(invoice_response.count).to eq(2)
+      expect(invoice_response.first['merchant_id']).to eq(invoice1.merchant_id)
+      expect(invoice_response.second['merchant_id']).to eq(invoice2.merchant_id)
+    end
+  end
+
 end
