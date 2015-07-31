c492308eb5f43d644f2ea664c79609f3d6579341
Jack Yeh <jack.yeh@gmail.com>
2015-07-30 21:09:15 -0600

    create/pass test: Invoice#random Invoice#find

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -29,4 +29,38 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
       expect(invoice['status']).to eq('fair')
     end
   end
+
+  context '#random' do
+    it 'returns random invoice' do
+      Invoice.create!(customer_id: 19, merchant_id: 81, status: "fair")
+      Invoice.create!(customer_id: 29, merchant_id: 82, status: "fair")
+      Invoice.create!(customer_id: 39, merchant_id: 83, status: "fair")
+      Invoice.create!(customer_id: 49, merchant_id: 84, status: "fair")
+      Invoice.create!(customer_id: 59, merchant_id: 85, status: "fair")
+      Invoice.create!(customer_id: 69, merchant_id: 86, status: "fair")
+      Invoice.create!(customer_id: 79, merchant_id: 87, status: "fair")
+      Invoice.create!(customer_id: 89, merchant_id: 88, status: "fair")
+      Invoice.create!(customer_id: 99, merchant_id: 89, status: "fair")
+      get :random, format: :json
+
+      expect(response).to have_http_status(:ok)
+      invoice_response= JSON.parse(response.body)
+      expect(invoice_response).not_to eq(Invoice.all.sample)
+    end
+  end
+
+  context '#find' do
+    it 'returns specific item' do
+      invoice1 = Invoice.create!(customer_id: 89, merchant_id: 88, status: "tbd")
+      invoice2 = Invoice.create!(customer_id: 99, merchant_id: 89, status: "fair")
+      get :find, customer_id: invoice2.customer_id
+
+      expect(response).to have_http_status(:ok)
+      invoice_response = JSON.parse(response.body)
+      expect(invoice_response['customer_id']).to eq(99)
+      expect(invoice_response['merchant_id']).to eq(89)
+      expect(invoice_response['status']).to eq('fair')
+    end
+  end
+
 end
