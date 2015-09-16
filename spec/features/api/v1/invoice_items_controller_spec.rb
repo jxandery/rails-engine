8d8d06bfa42710965c386b08a515a8a96a07f57a
Jack Yeh <jack.yeh@gmail.com>
2015-09-15 22:43:06 -0600

    refactor: the invoice items tests are more requests/features tests than they are controller tests - changing the uri to the hit the endpoint as a client/user would - changing the describe description and type - moving the tests into a features folder and out of the controllers folder

--- spec/features/api/v1/invoice_items_controller_spec.rb
+++ spec/features/api/v1/invoice_items_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::InvoiceItemsController, type: :controller do
+RSpec.describe "/api/v1/invoice_items", type: :request do
   context '#index' do
     it 'returns all the invoice_items' do
       InvoiceItem.create!(invoice_id: 99, item_id: 88, quantity: 100, unit_price: 77)
-      get :index, format: :json
+      get "/api/v1/invoice_items", format: :json
 
       expect(response).to have_http_status(:ok)
       invoice_items = JSON.parse(response.body)
@@ -21,7 +21,7 @@ RSpec.describe Api::V1::InvoiceItemsController, type: :controller do
   context '#show' do
     it 'returns individual invoice_item' do
       invoice_item = InvoiceItem.create!(invoice_id: 99, item_id: 88, quantity: 100, unit_price: 77)
-      get :show, id: invoice_item.id, format: :json
+      get "/api/v1/invoice_items/#{invoice_item.id}", format: :json
 
       expect(response).to have_http_status(:ok)
       invoice_item_response = JSON.parse(response.body)
@@ -43,8 +43,7 @@ RSpec.describe Api::V1::InvoiceItemsController, type: :controller do
       InvoiceItem.create!(invoice_id: 79, item_id: 78, quantity: 107, unit_price: 77)
       InvoiceItem.create!(invoice_id: 89, item_id: 88, quantity: 108, unit_price: 87)
       InvoiceItem.create!(invoice_id: 99, item_id: 98, quantity: 109, unit_price: 97)
-
-      get :random, format: :json
+      get "/api/v1/invoice_items/random", format: :json
 
       expect(response).to have_http_status(:ok)
       invoice_item_response= JSON.parse(response.body)
@@ -56,7 +55,7 @@ RSpec.describe Api::V1::InvoiceItemsController, type: :controller do
     it 'returns specific invoice_item' do
       invoice_item1 = InvoiceItem.create!(invoice_id: 19, item_id: 18, quantity: 119, unit_price: 17)
       invoice_item2 = InvoiceItem.create!(invoice_id: 99, item_id: 98, quantity: 109, unit_price: 97)
-      get :find, item_id: invoice_item2.item_id
+      get "/api/v1/invoice_items/find", item_id: invoice_item2.item_id, format: :json
 
       expect(response).to have_http_status(:ok)
       invoice_item = JSON.parse(response.body)
@@ -71,7 +70,7 @@ RSpec.describe Api::V1::InvoiceItemsController, type: :controller do
     it 'returns all invoice_item' do
       invoice_item1 = InvoiceItem.create!(invoice_id: 19, item_id: 18, quantity: 119, unit_price: 17)
       invoice_item2 = InvoiceItem.create!(invoice_id: 19, item_id: 98, quantity: 109, unit_price: 97)
-      get :find_all, invoice_id: 19
+      get "/api/v1/invoice_items/find_all", invoice_id: 19, format: :json
 
       expect(response).to have_http_status(:ok)
       invoice_item = JSON.parse(response.body)
@@ -83,7 +82,7 @@ RSpec.describe Api::V1::InvoiceItemsController, type: :controller do
     it 'returns specific invoice' do
       invoice = Invoice.create!(customer_id: 3, merchant_id: 7, status: "success")
       invoice_item1 = InvoiceItem.create!(invoice_id: invoice.id, item_id: 18, quantity: 119, unit_price: 17)
-      get :invoice, invoice_item_id: invoice_item1.id
+      get "/api/v1/invoice_items/#{invoice_item1.id}/invoice", invoice_id: 19, format: :json
 
       expect(response).to have_http_status(:ok)
       invoice_response = JSON.parse(response.body)
@@ -97,7 +96,7 @@ RSpec.describe Api::V1::InvoiceItemsController, type: :controller do
     it 'returns specific item' do
       item = Item.create!(name: 'item9', description: 'description9', unit_price: 99, merchant_id: 9)
       invoice_item1 = InvoiceItem.create!(invoice_id: 8, item_id: item.id, quantity: 119, unit_price: 17)
-      get :item, invoice_item_id: invoice_item1.id
+      get "/api/v1/invoice_items/#{invoice_item1.id}/item", invoice_id: 19, format: :json
 
       expect(response).to have_http_status(:ok)
       item_response = JSON.parse(response.body)
