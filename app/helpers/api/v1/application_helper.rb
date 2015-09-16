bff4535d9fcd7ea697a2879d6fbca628b939b0ea
Jack Yeh <jack.yeh@gmail.com>
2015-09-15 22:30:04 -0600

    add notes for future bug fix

--- app/helpers/api/v1/application_helper.rb
+++ app/helpers/api/v1/application_helper.rb
@@ -1,6 +1,13 @@
 module Api::V1::ApplicationHelper
 
   def successful_invoices
+    # may need to change this method
+    # currently invoice has many transactions
+    # those transactions can be both success and failed
+    #
+    # as the method is currently defined, if any of the transactions are successful,
+    # then the invoice is listed as successful
+    # may want to consider changing/writing the test and method to if any transactions.result == 'failed'
     Invoice.joins(:transactions).where(transactions: {result: 'success'})
   end
 
