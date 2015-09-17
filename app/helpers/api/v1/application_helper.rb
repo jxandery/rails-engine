90832c298c0be25c25e066db833d569116b0829c
Jack Yeh <jack.yeh@gmail.com>
2015-09-17 11:56:35 -0600

    refactor: extract out successful transactions

--- app/helpers/api/v1/application_helper.rb
+++ app/helpers/api/v1/application_helper.rb
@@ -1,24 +1,7 @@
 module Api::V1::ApplicationHelper
 
-  def successful_invoices(invoices)
-    invoices.joins(:transactions).where(transactions: {result: 'success'})
-  end
-
-  def successful_merchant_invoices
-    successful_invoices(find_merchant.invoices)
-  end
-
-  def customer_invoices
-    successful_merchant_invoices.group_by {|invoice| invoice.customer_id}
-  end
-
   def find_customer
     Customer.find_by(id: params[:customer_id])
-  end
-
-  def successful_customer_invoices
-    i = successful_invoices(find_customer.invoices)
-    i.group_by {|x| x.merchant_id}.first.last.first.merchant_id
   end
 
   def find_invoice_item
@@ -41,8 +24,25 @@ module Api::V1::ApplicationHelper
     Transaction.find_by(id: params[:transaction_id])
   end
 
+  def successful_invoices(invoices)
+    invoices.joins(:transactions).where(transactions: {result: 'success'})
+  end
+
   def invoice_total(invoice)
     invoice.invoice_items.map {|invoice_item| invoice_item.unit_price * invoice_item.quantity}.reduce(:+)
+  end
+
+  def successful_merchant_invoices
+    successful_invoices(find_merchant.invoices)
+  end
+
+  def customer_invoices
+    successful_merchant_invoices.group_by {|invoice| invoice.customer_id}
+  end
+
+  def successful_customer_invoices
+    i = successful_invoices(find_customer.invoices)
+    i.group_by {|x| x.merchant_id}.first.last.first.merchant_id
   end
 
   def revenue(invoices)
@@ -62,9 +62,8 @@ module Api::V1::ApplicationHelper
   end
 
   def all_invoices_by_date
-    i = Invoice.joins(:transactions).where(transactions: {result: 'success'})
-    x = i.find_all {|invoice| invoice.created_at.to_s[0..-5] == params[:date]}
-    x.map {|invoice| invoice_total(invoice)}.reduce(:+)
+    i = successful_invoices(Invoice.all).find_all {|invoice| invoice.created_at.to_s[0..-5] == params[:date]}
+    i.map {|invoice| invoice_total(invoice)}.reduce(:+)
   end
 
   def favorite_customer
@@ -76,14 +75,14 @@ module Api::V1::ApplicationHelper
   end
 
   def customers_with_pending_invoices
-    (Invoice.where(merchant_id: params["merchant_id"].to_i) - successful_merchant_invoices).map do |invoice|
+    successful_invoices(find_merchant.invoices).map do |invoice|
+    #(Invoice.where(merchant_id: params["merchant_id"].to_i) - successful_merchant_invoices).map do |invoice|
       Customer.find(invoice.customer_id)
     end
   end
 
   def item_count(merchant_id)
-    successful_invoices = Merchant.find(merchant_id).invoices.joins(:transactions).where(transactions: {result: 'success'})
-    successful_invoices.joins(:invoice_items).sum(:quantity)
+    successful_invoices(Merchant.find(merchant_id).invoices).joins(:invoice_items).sum(:quantity)
   end
 
   def most_items
