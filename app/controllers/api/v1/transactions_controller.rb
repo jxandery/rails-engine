5cce3cec74a063bb3a80a2c4655256d5fe0bafc6
Jack Yeh <jack.yeh@gmail.com>
2015-09-16 19:30:22 -0600

    fix bug: add :id to & remove :expiration_date from find_params

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -23,6 +23,6 @@ class Api::V1::TransactionsController < Api::V1::ApplicationController
   private
 
   def find_params
-    params.permit(:invoice_id, :credit_card_number, :credit_card_expiration, :result, :created_at, :updated_at)
+    params.permit(:id, :invoice_id, :credit_card_number, :result, :created_at, :updated_at)
   end
 end
